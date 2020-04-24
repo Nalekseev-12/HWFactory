d7f33b1783d3ce208c3e65feec0f76178ece34c4
Nalekseev-12 <alekse-nikita@yandex.ru>
2020-04-24 10:42:59 +0300

    fixed mvn clean test

--- src/main/java/factory/WebDriverFactory.java
+++ src/main/java/factory/WebDriverFactory.java
@@ -17,33 +17,36 @@ public class WebDriverFactory {
 
 
     public WebDriver create(String webDriverName, String... options) {
-        WebDriver driver = null;
-        if (enumName.CHROME.name().equalsIgnoreCase(webDriverName)) {
-            if (options != null) {
-                WebDriverManager.chromedriver().setup();
-                ChromeOptions chromeOptions = new ChromeOptions();
-                chromeOptions.addArguments(options);
-                driver = new ChromeDriver(chromeOptions);
+
+            WebDriver driver;
+            if (enumName.CHROME.name().equalsIgnoreCase(webDriverName)) {
+                if (options != null) {
+                    WebDriverManager.chromedriver().setup();
+                    ChromeOptions chromeOptions = new ChromeOptions();
+                    chromeOptions.addArguments(options);
+                    driver = new ChromeDriver(chromeOptions);
+                } else {
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                }
+            } else if (enumName.FIREFOX.name().equalsIgnoreCase(webDriverName)) {
+                if (options != null) {
+                    WebDriverManager.firefoxdriver().setup();
+                    FirefoxOptions firefoxOptions = new FirefoxOptions();
+                    firefoxOptions.addArguments(options);
+                    driver = new FirefoxDriver();
+                } else {
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                }
+
             } else {
-                WebDriverManager.chromedriver().setup();
-                driver = new ChromeDriver();
-            }
-        } else if (enumName.FIREFOX.name().equalsIgnoreCase(webDriverName)) {
-            if (options != null) {
-                WebDriverManager.firefoxdriver().setup();
-                FirefoxOptions firefoxOptions = new FirefoxOptions();
-                firefoxOptions.addArguments(options);
-                driver = new FirefoxDriver();
-            } else {
-                WebDriverManager.firefoxdriver().setup();
-                driver = new FirefoxDriver();
+                driver = null;
             }
 
-        } else {
-            driver = null;
+            return driver;
+
         }
-        return driver;
-
-
-    }
 }
+
+
--- src/test/java/TestFactory.java
+++ src/test/java/TestFactory.java
@@ -1,16 +1,25 @@
+import com.beust.jcommander.Parameter;
 import factory.WebDriverFactory;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import java.util.Properties;
 
 public class TestFactory {
 
 
 
-@Test
+    @Test
     public void webBrowser() {
-    String browser = System.getProperty("browser");
-    WebDriver wd = new WebDriverFactory().create(browser);
-    wd.get("https://otus.ru/");
-}
+       String browser = System.getProperty("browser");
+        WebDriver wd = new WebDriverFactory().create(browser);
+        if (wd == null) {
+            wd = new WebDriverFactory().create("chrome");
+        }
+        wd.get("https://otus.ru/");
+    }
 }
